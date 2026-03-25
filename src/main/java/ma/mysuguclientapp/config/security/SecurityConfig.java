c2e3b7831c89077f2e0ef08778aa957aa2c020e5
Issiaka traoré <issiaka.traore@systalink.fr>
2026-03-25 02:41:54 +0000

    Update CORS config

--- src/main/java/ma/mysuguclientapp/config/security/SecurityConfig.java
+++ src/main/java/ma/mysuguclientapp/config/security/SecurityConfig.java
@@ -191,11 +191,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://localhost:4200",
-                "http://localhost:5173", "http://localhost:5174", "*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
         configuration.setAllowCredentials(false);
         configuration.setMaxAge(3600L);
 
